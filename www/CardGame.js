71f8390701100fedc33c68a6a5a1f40f8adba42b
Administrator <Administrator@MSDN-SPECIAL>
2014-03-17 16:35:51 +0900

    .

--- www/CardGame.js
+++ www/CardGame.js
@@ -6,7 +6,7 @@ var gameImg = [ "apple.jpg", "banana.jpg", "grape.jpg", "kiwi.jpg",
 		"1.jpg", "2.jpg", "3.jpg", "4.jpg",
 		"5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg",
 		"10.jpg", "11.jpg", "12.jpg" , "13.jpg" , "14.jpg" ,
-		"15.jpg" , "16.jpg" , "17.jpg" , , "18.jpg" , "19.jpg",
+		"15.jpg" , "16.jpg" , "17.jpg" , "18.jpg" , "19.jpg",
 		"20.jpg" , "21.jpg" , "22.jpg" , "23.jpg"];
 
 var makeNumber = 0;
